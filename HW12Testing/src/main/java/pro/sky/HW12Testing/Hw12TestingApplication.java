163333830aaca2e05c67bb9ab134b986f1167ba9
MaraOD555 <ruffbc@hotmail.com>
2022-08-22 17:56:17 +0300

    Master

---
+++ HW12Testing/src/main/java/pro/sky/HW12Testing/Hw12TestingApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.HW12Testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw12TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw12TestingApplication.class, args);
+	}
+
+}
---
+++ HW12Testing/src/test/java/pro/sky/HW12Testing/Hw12TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.HW12Testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw12TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
